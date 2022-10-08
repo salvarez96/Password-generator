ed3c361961963c1bde01ddecd068b8134b369cbe
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-10-07 19:30:21 -0500

    Finish most of the app's functionality
    Add titles to the inputs to aid accesibility. Only the copy button is left to add functionality

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
-import {showPasswordLength, passwordLengthSlider} from './nodes.js';
+import {passwordText} from './nodes.js';
 
 let letters = 'abcdefghijklmnopqrstuvwxyz';
-letters += letters.toUpperCase();
-letters += '1234567890';
+letters += letters.toUpperCase() + '1234567890';
 
 const symbols = '!#$%&/()[].,;<>?¿¡+*-=';
 
-// const allCharactersArr = letters.split('')
-// console.log(allCharactersArr);
-
 const randomPassword = (passwordLength) => {
-  //minimun password length = 6
   const minLength = 6;
   const randomNumArr = [];
   let randomNum = 0;
@@ -19,12 +14,11 @@ const randomPassword = (passwordLength) => {
       randomNum = Math.floor(Math.random() * (letters.length));
       randomNumArr.push(letters[randomNum]);
     }
-
     console.log(randomNumArr);
-    return randomNumArr.join('');
+    passwordText.innerText = randomNumArr.join('');
   } else {
     return 'Your password should be at least 6 characters long';
   }
 }
 
-console.log(randomPassword(6));
+export {randomPassword}
--- src/nodes.js
+++ src/nodes.js
@@ -1,9 +1,23 @@
-const showPasswordLength = document.querySelector('.length-container__text--num-value');
-const passwordLengthSlider = document.querySelector('.length-container__slider');
+import { randomPassword } from "./main.js";
+
+const getElements = (idOrClass) => document.querySelector(idOrClass);
+
+const showPasswordLength = getElements('.length-container__text--num-value');
+const passwordLengthSlider = getElements('.length-container__slider');
+const passwordText = getElements('.password-container__text');
+const passwordCopy = getElements('.password-container__copy');
+const generateButton = getElements('#generate-password');
 
 showPasswordLength.innerText = passwordLengthSlider.value;
+
+let passwordLength = passwordLengthSlider.value;
 passwordLengthSlider.addEventListener('input', () => {
   showPasswordLength.innerText = passwordLengthSlider.value;
+  passwordLength = passwordLengthSlider.value;
 });
 
-export {showPasswordLength, passwordLengthSlider};
+document.addEventListener('DOMContentLoaded', randomPassword(passwordLength));
+
+generateButton.addEventListener('click', () => randomPassword(passwordLength));
+
+export {showPasswordLength, passwordLengthSlider, passwordCopy, passwordText, generateButton, passwordLength};
